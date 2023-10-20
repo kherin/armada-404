c7a05af323065bac056b70d4fd5ac548879c497f
bilaal-inpleo <bilaal.abdel.hassan@inpleo.com>
2023-10-20 15:01:48 +0400

    react

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
+const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
 });
 
 app.get('/status', (req, res) => {
