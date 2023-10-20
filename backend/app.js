110c101e7a6c14ae2ae7dd92b3e6d32b96bd7f34
bilaal-inpleo <bilaal.abdel.hassan@inpleo.com>
2023-10-20 15:29:44 +0400

    Changed Backend Port to 3001

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
